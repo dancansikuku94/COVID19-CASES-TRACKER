73064e02cc95cd6408fe122e2f8c2af5b739968c
Dancan sikuku <dancansikuku94@gmail.com>
2021-07-02 13:11:32 +0300

    first commit

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -12,21 +12,21 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 export default {
-  name: 'CountrySelect',
-  props: ['countries'],
-  setup ({ countries }, { emit }) {
+  name: "CountrySelect",
+  props: ["countries"],
+  setup({ countries }, { emit }) {
     const selected = ref(0);
 
     return {
       selected,
-      onChange () {
+      onChange() {
         const country = countries.find((item) => item.ID === selected.value);
-        emit('get-country', country);
-      }
+        emit("get-country", country);
+      },
     };
-  }
+  },
 };
 </script>
