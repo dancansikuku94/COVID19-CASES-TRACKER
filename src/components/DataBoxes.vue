22de4139f3c3a915322f46b4d58d07b34b20e4bc
Dancan sikuku <dancansikuku94@gmail.com>
2021-07-02 13:53:09 +0300

    Initial commit to the repo

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -2,10 +2,10 @@
   <div class="grid md:grid-cols-2 gap-4">
     <!-- Box 1 -->
     <div class="shadow-md bg-blue-100 p-10 text-center rounded">
-      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Covid-19 Cases</h3>
 
       <div class="text-2xl mb-4">
-        <span class="font-bold">New:</span>
+        <span class="font-bold">New Covid-19 Cases:</span>
         {{ numberWithCommas(stats.NewConfirmed) }}
       </div>
       <div class="text-2xl mb-4">
@@ -15,8 +15,8 @@
     </div>
 
     <!-- Box 2 -->
-    <div class="shadow-md bg-blue-200 p-10 text-center rounded">
-      <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+    <div class="shadow-md bg-gray-200 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Covid-19 Deaths</h3>
 
       <div class="text-2xl mb-4">
         <span class="font-bold">New:</span>
